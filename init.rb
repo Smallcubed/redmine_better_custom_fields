09a1b95862652043dfea5e535a7d8d5e194d4bae
Scott Little <scott@littleknownsoftware.com>
2019-11-04 12:23:14 +0000

    Update version number

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_better_custom_fields do
   name 'Redmine Better Custom Field API'
   author 'SmallCubed Inc.'
   description 'This plugin provides a better, more complete API for custom fields' 
-  version '0.1.0'
+  version '0.2.0'
   url 'https://github.com/smallcubed/redmine_better_custom_fields'
   author_url 'http://smallcubed.com'
   requires_redmine :version_or_higher => '2.4.0'
