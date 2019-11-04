981d30040211b5980267a063e752ed3395fa252e
Scott Little <scott@littleknownsoftware.com>
2019-11-04 15:57:45 +0000

    Remove global(?) scope of variables that wasn't needed

--- app/controllers/redmine_better_issue_transitions_controller.rb
+++ app/controllers/redmine_better_issue_transitions_controller.rb
@@ -7,13 +7,13 @@ class RedmineBetterIssueTransitionsController < ApplicationController
   def index
     respond_to do |format|      
       format.api {
-      	@roles = User.current.roles
-        @issue = Issue.find(params[:issue_id])
+      	roles = User.current.roles
+        issue = Issue.find(params[:issue_id])
         @allowed_transitions = []
-        if @roles
-          @workflow = WorkflowTransition.where(:tracker_id => @issue.tracker, :role_id => @roles)
-          @workflow.each do |transition|
-          	if transition.old_status_id == @issue.status_id
+        if roles
+          workflow = WorkflowTransition.where(:tracker_id => issue.tracker, :role_id => roles)
+          workflow.each do |transition|
+          	if transition.old_status_id == issue.status_id
           	  @allowed_transitions.push(transition.new_status_id)
           	end
           end
@@ -23,7 +23,7 @@ class RedmineBetterIssueTransitionsController < ApplicationController
   end
   
   def find_issue
-    @issue = Issue.find(params[:issue_id])
+    issue = Issue.find(params[:issue_id])
   rescue ActiveRecord::RecordNotFound
     render_404
   end
