59735d6b9f525d33cd5c0d075146f8d7d6783180
Scott Little <scott@littleknownsoftware.com>
2019-11-04 16:17:12 +0000

    Remove the 404 for an issue not found and change layout

--- app/controllers/redmine_better_issue_transitions_controller.rb
+++ app/controllers/redmine_better_issue_transitions_controller.rb
@@ -1,6 +1,5 @@
 class RedmineBetterIssueTransitionsController < ApplicationController
 
-  before_filter :find_issue
   before_filter :require_login
   accept_api_auth :index
 
@@ -8,12 +7,12 @@ class RedmineBetterIssueTransitionsController < ApplicationController
     respond_to do |format|      
       format.api {
       	roles = User.current.roles
-        issue = Issue.find(params[:issue_id])
+        @issue = Issue.find(params[:issue_id])
         @allowed_transitions = []
-        if roles
-          workflow = WorkflowTransition.where(:tracker_id => issue.tracker, :role_id => roles)
+        if @issue && roles
+          workflow = WorkflowTransition.where(:tracker_id => @issue.tracker, :role_id => roles)
           workflow.each do |transition|
-          	if transition.old_status_id == issue.status_id
+          	if transition.old_status_id == @issue.status_id
           	  @allowed_transitions.push(transition.new_status_id)
           	end
           end
@@ -22,10 +21,4 @@ class RedmineBetterIssueTransitionsController < ApplicationController
     end
   end
   
-  def find_issue
-    issue = Issue.find(params[:issue_id])
-  rescue ActiveRecord::RecordNotFound
-    render_404
-  end
-
 end
